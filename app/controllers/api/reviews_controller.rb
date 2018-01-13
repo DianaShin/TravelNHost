e7f6525ed71ddbd017824b9cbefbd4a8c65ef6f6
Diana Shin <ayoung0131@gmail.com>
2018-01-13 15:49:15 -0500

    Add reviews controller and routes

---
+++ app/controllers/api/reviews_controller.rb
@@ -0,0 +1,45 @@
+class Api::ReviewsController < ApplicationController
+
+  def new
+    @review = Review.new
+  end 
+
+  def create
+    @review = Review.new(review_params)
+    @review.guest_id = current_user.id
+    if @review.save
+      render :show
+    else
+      render json: @review.error.full_messages, status: 422
+    end
+  end
+
+  def index
+    @reviews = Review.where(:guest_id: params[:user_id])
+  end
+
+  def edit
+    @review = Review.find(params[:id])
+  end
+
+  def update
+    @review = Review.find(params[:id])
+    if @review.update(review_params)
+      render :show
+    else
+      render json: @review.errors.full_messages, status: 422
+    end
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy!
+    render :show
+  end
+
+  private
+  def review_params
+    params.require(:review).
+    permit(:host_id, :guest_id, :title, :body)
+  end
+end
---
+++ app/views/api/reviews/_review.json.jbuilder
@@ -0,0 +1,6 @@
+json.extract! review, :id, :host_id, :guest_id, :title, :body,
+guest = review.guest
+
+json.author_name guest.first_name.concat(' ').concat(guest.last_name)
+json.author_location guest.destination.name.concat(', ').concat(guest.destination.country)
+json.author_id guest.id
